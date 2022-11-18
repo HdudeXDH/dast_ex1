7a8162bce7e1bebf62f9b52827cff569dcbfa742
alofeld@gmail.com <Depo2mypro!@#>
2022-11-18 16:29:05 +0200

    initial commit with BinaryTree.h

--- BinaryTree.h
+++ BinaryTree.h
@@ -1,38 +1,52 @@
 #ifndef DAST_EX1_BINARYTREE_H
 #define DAST_EX1_BINARYTREE_H
 
-template <typename T>
-class Node<T> {
+template <typename T, typename V>
+class Node {
 public:
-	T data;
-	Node<T>* left;
-	Node<T>* right;
-	Node<T>* parent;
+    Node(const T& key, const V& value, Node<T, V>* parent= nullptr, int height=0):
+    key(key), value(value),parent(parent),height(height)
+     {
+       left= nullptr;
+       right= nullptr;
+    }
+    int key;
+    V value;
+	Node<T, V>* parent,left, right;
+    int height;
 };
 
 
-template <typename T>
-class BinaryTree<T> {
-
-protected:
-	Node<T> root
-
+template <typename T,typename V>
+class BinaryTree {
+//protected:
 public:
-	BinaryTree();
-	~BinaryTree();
-	search(const T& object);
-	virtual add(const T& object)= 0;
-	virtual remove(const T& object) = 0;
-	operator>(BinaryTree<T> other)
+    Node<T, V> * root;
+
+    BinaryTree();
+    ~BinaryTree();
+    Node<T, V>& search(const T & target_key) {
+        if (root== nullptr) return nullptr;
+        if (root->key==target_key){ return this; }
+        if (root->key<target_key) {
+            if (root->right== nullptr) return nullptr;}
+    };
+    int height(){
+        return root->height;
+    };
+    int bf(){
+        return root->left.height-root->right.height;
+    };
+
+	void virtual add(const T& key, const V& value )= 0;
+	void virtual remove(const T& key) = 0;
+	bool friend operator>(const Node<T, V> & first, const Node<T, V> & second);
 
 };
-template<typename T>
-public operator>(BinaryTree<T> first, BinaryTree<T> second) {
+template<typename T, typename V>
+bool operator>(const Node<T, V> & first, const Node<T, V> & second) {
 	return first.key > second.key;
 }
-//
-//public operator>(BinaryTree<T> other) {
-//
-//}
+
 
 #endif //DAST_EX1_BINARYTREE_H

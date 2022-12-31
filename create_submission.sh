b5f190e238231e67453e3044ec21233026d6c07c
alofeld@gmail.com <Depo2mypro!@#>
2022-12-31 16:50:26 +0200

    adding submission

---
+++ create_submission.sh
@@ -0,0 +1,2 @@
+rm test_submission/our_submission.zip
+zip test_submission/our_submission.zip AVLTree.h LinkedList.h Player.cpp Player.h submissions.txt dry.pdf Team.cpp Team.h worldcup23a1.cpp worldcup23a1.h
